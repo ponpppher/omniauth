4538f05137888c30e38a78e5c5a4527e1c204fa7
ponpppher <genmaier@gmail.com>
2019-04-30 02:53:58 +0900

    test implements omniauth

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'pages#index'
+  get 'pages/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+end
